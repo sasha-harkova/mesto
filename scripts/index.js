cb8da3f82f5e63cfe238ed3a7719c45afd65c238
Sasha Kharkova <alexandra.filipceva@yandex.ru>
2022-09-20 17:31:41 +0300

    fix: исправила все замечания

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,16 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupSaveButton = popup.querySelector('.popup__save-button');
 const popupCloseButton = popup.querySelector('.popup__close-button');
-let nameInput = popup.querySelector('.popup__form-item_name');
-let jobInput = popup.querySelector('.popup__form-item_job');
+let nameInput = popup.querySelector('.popup__form-item_el_name');
+let jobInput = popup.querySelector('.popup__form-item_el_job');
 let userName = document.querySelector('.profile__username');
 let userDescription = document.querySelector('.profile__user-description');
-let formElement = popup.querySelector('.popup__container');
+let formElement = popup.querySelector('.popup__form');
 
 function popupOpen() {
   popup.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userDescription.textContent;
 }
 
 function popupClose() {
@@ -23,7 +25,6 @@ function popupCloseByClickingOverlay(event) {
 }
 
 popupOpenButton.addEventListener('click', popupOpen);
-popupSaveButton.addEventListener('click', popupClose);
 popupCloseButton.addEventListener('click', popupClose);
 popup.addEventListener('click', popupCloseByClickingOverlay);
 
@@ -33,6 +34,7 @@ function popupSave(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userDescription.textContent = jobInput.value;
+  popupClose();
 }
 
 formElement.addEventListener('submit', popupSave);
