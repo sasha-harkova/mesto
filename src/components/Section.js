e1774000d8943d6a5c575b03acad99ff4aad7e4c
Sasha Kharkova <alexandra.filipceva@yandex.ru>
2022-11-30 21:56:52 +0300

    feat: подключила проект к серверу

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,15 +1,18 @@
 export default class Section {
-  constructor({ items, renderer }, containerSelector) {
-    this._renderedItems = items;
+  constructor({ renderer }, containerSelector) {
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
-  renderItems() {
-    this._renderedItems.forEach((item) => this._renderer(item));
+  renderItems(items) {
+    items.forEach((item) => this._renderer(item));
   }
 
   addItem(card) {
+    this._container.append(card);
+  }
+
+  addNewItem(card) {
     this._container.prepend(card);
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 import FormValidator from "../components/FormValidator.js";
-import initialCards from "../utils/cards.js";
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
@@ -16,30 +15,92 @@ import {
   newCardForm,
   newAvatarForm,
   avatarInput,
-  objForValidation
+  objForValidation,
 } from "../utils/constans.js"
+
+import Api from "../components/Api.js"
+
+const api = new Api({
+  url: 'https://mesto.nomoreparties.co/v1/cohort-55',
+  headers: {
+    authorization: 'abb2bbf6-61b5-4346-b7aa-47dedd2bc449',
+    'Content-Type': 'application/json'
+  }
+});
+
+//------------ФУНКЦИЯ ЗАГРУЗКИ ОТВЕТА ОТ СЕРВЕРА------------//
+
+function renderLoading(isLoading, popup) {
+  const popupSaveButton = popup._form.querySelector('.popup__save-button');
+  if(isLoading) {
+    popupSaveButton.textContent = 'Сохранение...'
+  } else {
+    popupSaveButton.textContent = 'Сохранить'
+  }
+}
+
+
+//------------ПОЛУЧЕНИЕ ИНФОРМАЦИИ ПОЛЬЗОВАТЕЛЯ С СЕРВЕРА------------//
+
+let userId;
+
+Promise.all([api.getUserInfoAndAvatar, api.getInitialCards])
+.then(() => {
+  api.getUserInfoAndAvatar()
+    .then((user) => {
+      userInfo.setUserInfo(user);
+      userInfo.setUserAvatar(user);
+      userId = user._id;
+    })
+    .catch((error) => console.log(`Ошибка при получении информации пользователя с сервера: ${error}`));
+});
 
 
 //------------УНИВЕРСАЛЬНАЯ ФУНКЦИЯ ДОБАВЛЕНИЯ КАРТОЧКИ------------//
 
-function createCard(photo, caption) {
-  const card = new Card(photo, caption, ".template-add-photo", { handleCardClick: () => {
-    popupWithImage.open(photo, caption);
+const addLike = (id) => api.addLike(id);
+const removeLike = (id) => api.deleteLike(id);
+
+let deleteCardFromServer;
+
+const popupAreYouSure = new PopupWithForm(".popup_type_are-you-sure", { handleFormSubmit: () => deleteCardFromServer() });
+popupAreYouSure.setEventListeners();
+
+const deleteCard = (id, deleteElement) => {
+  popupAreYouSure.open();
+  deleteCardFromServer = () => {
+    renderLoading(true, popupAreYouSure);
+    api.deleteCard(id)
+      .then(() => deleteElement())
+      .catch((error) => console.log(`Ошибка при удалении карточки: ${error}`))
+      .finally(() => renderLoading(false, popupAreYouSure))
+  };
+};
+
+function createCard(data) {
+  const card = new Card(data, ".template-add-photo", userId, addLike, removeLike, deleteCard, { handleCardClick: () => {
+    popupWithImage.open(data);
   } });
   const cardElement = card.createCard();
   return cardElement;
 }
 
+
 //------------ДОБАВЛЕНИЕ КАРТОЧЕК ИЗ МАССИВА------------//
 
 const cardsContainer = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    const cardElement = createCard(item.link, item.name);
-    cardsContainer.addItem(cardElement)
+  renderer: (card) => {
+    const cardElement = createCard(card);
+    cardsContainer.addItem(cardElement);
   }
-},'.content');
-cardsContainer.renderItems();
+}, '.content')
+
+api.getInitialCards()
+  .then((cards) => {
+    cardsContainer.renderItems(cards);
+  })
+  .catch((error) => console.log(`Ошибка при добавлении карточек с сервера: ${error}`));
+
 
 //------------ВАЛИДАЦИЯ ФОРМ------------//
 
@@ -52,34 +113,53 @@ validationProfileForm.enableValidation();
 const validationNewAvatarForm = new FormValidator(objForValidation, newAvatarForm);
 validationNewAvatarForm.enableValidation();
 
+
 //------------ПОПАП С ЗУМОМ ИЗОБРАЖЕНИЯ------------//
 
 const popupWithImage = new PopupWithImage('.popup_type_for-image');
 popupWithImage.setEventListeners();
+
 
 //------------ПОПАП РЕДАКТИРОВАНИЯ ПРОФИЛЯ------------//
 
 const userInfo = new UserInfo({usernameSelecor: ".profile__username", aboutSelector: ".profile__user-description", avatarSelector: ".profile__avatar"});
 
 const popupEditProfile = new PopupWithForm(".popup_type_edit-profile", { handleFormSubmit: () => {
-  userInfo.setUserInfo(nameInput, aboutInput);
+  renderLoading(true, popupEditProfile);
+  api.setUserInfo({ name: nameInput.value, about: aboutInput.value })
+    .then((user) => userInfo.setUserInfo(user))
+    .catch((error) => console.log(`Ошибка при изменении информации пользователя: ${error}`))
+    .finally(() => renderLoading(false, popupEditProfile))
 } });
 popupEditProfile.setEventListeners();
+
 
 //------------ПОПАП РЕДАКТИРОВАНИЯ АВАТАРА------------//
 
 const popupEditAvatar = new PopupWithForm(".popup_type_edit-avatar", { handleFormSubmit: () => {
-  userInfo.setAvatar(avatarInput);
+  renderLoading(true, popupEditAvatar);
+  api.setAvatar( { avatar: avatarInput.value } )
+    .then((user) => userInfo.setUserAvatar(user))
+    .catch((error) => console.log(`Ошибка при изменении аватара пользователя: ${error}`))
+    .finally(() => renderLoading(false, popupEditAvatar))
 } });
 popupEditAvatar.setEventListeners();
+
 
 //------------ПОПАП ДОБАВЛЕНИЯ НОВОЙ КАРТОЧКИ------------//
 
 const popupAddCard = new PopupWithForm(".popup_type_add-card", { handleFormSubmit: (formData) => {
-  const cardElement = createCard(formData.cardlink, formData.cardname);
-  cardsContainer.addItem(cardElement);
+  renderLoading(true, popupAddCard);
+  api.addNewCard({ name: formData.cardname, link: formData.cardlink })
+  .then((card) => {
+    const cardElement = createCard(card);
+    cardsContainer.addNewItem(cardElement);
+  })
+  .catch((error) => console.log(`Ошибка при добавлении новой карточки: ${error}`))
+  .finally(() => renderLoading(false, popupAddCard))
 } });
 popupAddCard.setEventListeners();
+
 
 //------------УНИВЕРСАЛЬНАЯ ФУНКЦИЯ ДЛЯ ПОПАПОВ С ФОРМАМИ------------//
 
